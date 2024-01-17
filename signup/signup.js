170ad8273c7067aacde2052edb1a7fa373f3b417
Taiyaba khan <taiyabak20@gmail.com>
2024-01-17 06:44:55 +0530

    signup frontend

---
+++ signup/signup.js
@@ -0,0 +1,16 @@
+const url = `http://localhost:3000/signup`
+const signup = document.querySelector('#Signup')
+signup.addEventListener('submit', createUser)
+
+async function createUser(e){
+    e.preventDefault();
+    const data = {
+        name: e.target.name.value,
+        email: e.target.email.value,
+        pnumber: e.target.pnumber.value,
+        password: e.target.password.value,
+    }
+    console.log(data)
+    //const res = axios.post(url, data)
+
+}
